a18fc24943087a091a1b9b4eb2635d3917c301df
eaintthetttun <eaintthetttun2013@gmail.com>
2025-07-05 12:02:48 +0630

    added testimonials section

--- js/script.js
+++ js/script.js
@@ -9,4 +9,22 @@ if ('serviceWorker' in navigator) {
         });
     });
   }
+
+  const menuBtn=document.querySelector('.menu');
+  // console.log('menuBtn',menuBtn);
+  const navItem=document.querySelector('.nav-items');
+  // console.log('navItem',navItem);
+  menuBtn.addEventListener('click',function(){
+    // alert('Menu btn clicked');
+    // console.log('hello click');
+    navItem.classList.toggle('show');
+    if(navItem.classList.contains('show')){
+      menuBtn.src='./photos/icons/cross.svg';
+    }
+    else{
+      menuBtn.src='./photos/icons/menu.svg';
+    }
+  })
+
+
   
